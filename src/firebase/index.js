681880605b76f5fb48226287b18d2f1e2d819b28
apd <andrewcodes404@gmail.com>
2018-12-12 16:59:26 +0000

    make env vars the same for dev and prod

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const prodConfig = {
-    apiKey: process.env.REACT_APP_PROD_API_KEY,
-    authDomain: process.env.REACT_APP_PROD_AUTHDOMIAN,
-    databaseURL: process.env.REACT_APP_PROD_DB_URL,
-    projectId: process.env.REACT_APP_PROD_PROJECT,
-    storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_PROD_MESSY_ID
+    apiKey: process.env.REACT_APP_DEV_API_KEY,
+    authDomain: process.env.REACT_APP_DEV_AUTHDOMIAN,
+    databaseURL: process.env.REACT_APP_DEV_DB_URL,
+    projectId: process.env.REACT_APP_DEV_PROJECT,
+    storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_DEV_MESSY_ID
 };
 
 const devConfig = {
