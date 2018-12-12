20119302ddd19f7160de13506fa3a352abe7309f
apd <andrewcodes404@gmail.com>
2018-12-12 16:52:47 +0000

    prima

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { db } from './firebase';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { users: null }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData = () => {
+    db.ref('user').once('value')
+      .then((snapshot) => {
+        const snap = snapshot.val()
+        let arrayOfUsers = []
+
+        Object.keys(snap).map((key) => {
+          console.log("snap[key].id : ", snap[key].name);
+
+          const name = snap[key].name
+          // snap[key].order.map((el)=>{
+          //     console.log("el : ", el);
+          // })      
+          arrayOfUsers.push(name)
+          return arrayOfUsers
+        })
+
+        this.setState({
+          users: arrayOfUsers
+        })
+      });
+  }
+
+  render() {
+    if (!this.state.users) return null
+    return (
+      <div className="App">
+        <h1>kaplunk</h1>
+        <div>
+          <h1>yes users</h1>
+          {this.state.users.map((el, index) => (<p key={index}>{el}</p>))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
